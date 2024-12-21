379a75a1ff25d063d32a7b83ad7c65b0994bfeda
xchxtom <103757778+xchxtom@users.noreply.github.com>
2024-12-21 17:10:43 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -1,9 +1,9 @@
 # 修改默认IP & 固件名称
 sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='.*'/hostname='Roc'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='XWRT'/g" package/base-files/files/bin/config_generate
 
 # 修改亚瑟运行LED为绿色
-# sed -i 's/&status_blue/\&status_green/g' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6000-re-ss-01.dts
+sed -i 's/&status_blue/\&status_green/g' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6000-re-ss-01.dts
 
 # 修改太乙启动LED为绿色，运行LED为蓝色(VIKINGYFY项目)
 # sed -i 's/boot/roc/g; s/running/boot/g; s/roc/running/g' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6010-re-cs-07.dts
